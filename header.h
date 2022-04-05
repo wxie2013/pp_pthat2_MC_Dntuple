d1582a67a28bab15a295112830efea6737683a1d
wxie2013 <wxie@cern.ch>
2022-04-05 12:03:50 -0400

    add BDTG branch to Lc MC with pt_hat2 in pp

---
+++ header.h
@@ -0,0 +1,2 @@
+
+#include  "/home/xiao147/private/TMVA_lambdaC/CMSSW_7_5_8_patch3/src/tmvaDntupleRunII_try_BDT_20172018/myTMVA/weights/TMVAClassification_pp_3_100_BDTG_5_cutvariables_0_1_2_3_5_log_test_weights/TMVAClassification_BDTG.class.C"
